526f84eeaa302d0832c01dfefe0a4ef555b451cb
bernardocarrilho <51291636+bernardocarrilho@users.noreply.github.com>
2022-12-13 10:10:25 +0000

    Add files via upload

--- temp/network/run_setup_network.sh
+++ temp/network/run_setup_network.sh
@@ -31,18 +31,18 @@ sudo ip netns exec ws10 ip route add default via 192.168.1.110 dev veth-ws10
 
 # Set Delays
 ## Add Simple Delay
-sudo tc qdisc add dev veth-br0-ws1 root netem delay 100ms
-sudo tc qdisc add dev veth-br0-ws2 root netem delay 200ms
-sudo tc qdisc add dev veth-br0-ws3 root netem delay 300ms
-sudo tc qdisc add dev veth-br0-ws4 root netem delay 400ms
-sudo tc qdisc add dev veth-br0-ws5 root netem delay 500ms
-sudo tc qdisc add dev veth-br0-ws6 root netem delay 600ms
-sudo tc qdisc add dev veth-br0-ws7 root netem delay 700ms
-sudo tc qdisc add dev veth-br0-ws8 root netem delay 800ms
-sudo tc qdisc add dev veth-br0-ws9 root netem delay 900ms
-sudo tc qdisc add dev veth-br0-ws10 root netem delay 1000ms
+#sudo tc qdisc add dev eth0 root netem delay 100ms
 ## Add Delay with Variability
-#sudo tc qdisc change dev eth0 root netem delay 100ms 10ms
+sudo tc qdisc add dev veth-br0-ws1 root netem delay 19.890ms 0.778ms
+sudo tc qdisc add dev veth-br0-ws2 root netem delay 19.937ms 0.660ms
+sudo tc qdisc add dev veth-br0-ws3 root netem delay 145.145ms 30.367ms
+sudo tc qdisc add dev veth-br0-ws4 root netem delay 179.132ms 13.756ms
+sudo tc qdisc add dev veth-br0-ws5 root netem delay 34.046ms 2.223ms
+sudo tc qdisc add dev veth-br0-ws6 root netem delay 225.656ms 38.514ms
+sudo tc qdisc add dev veth-br0-ws7 root netem delay 11.771ms 0.946ms
+sudo tc qdisc add dev veth-br0-ws8 root netem delay 11.498ms 1.172ms
+sudo tc qdisc add dev veth-br0-ws9 root netem delay 11.996ms 1.252ms
+sudo tc qdisc add dev veth-br0-ws10 root netem delay 171.547ms 33.674ms
 ## Add Delay with Correlation
 #sudo tc qdisc change dev eth0 root netem delay 100ms 10ms 25%
 ## Add Delay Distribution
